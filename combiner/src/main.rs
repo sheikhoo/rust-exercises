e08ec1bf9685b0fcbf3bdcc923432d5bea80ed46
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2023-10-06 23:09:31 +0330

    combiner images proj

---
+++ combiner/src/main.rs
@@ -0,0 +1,18 @@
+mod args;
+use args::Args;
+use image::{io::Reader, ImageFormat, DynamicImage};
+use std::{io::BufReader, fs::File};
+
+fn main() {
+    let args = Args::new();
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+    println!("{:?}", args);
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
+}
