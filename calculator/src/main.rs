b574fd07da1e6b2de2e81ebe21f3229c558dd5fa
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2023-10-06 17:05:24 +0330

    use `match` instead of `if-else` and use panic!

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -8,19 +8,19 @@ fn main() {
     let second_num: f32 = args.nth(0).unwrap().parse::<f32>().unwrap();
     let result: f32 = operate(oprator, fisr_num, second_num);
 
-    println!("{} {} {} = {}",fisr_num,oprator,second_num,result);
+    println!("{}",output(fisr_num,oprator,second_num,result));
 }
 
 fn operate(oprator: char,fisr_num: f32,second_num: f32) -> f32 {
-    if oprator == '+' {
-        fisr_num + second_num
-    } else if oprator == '-' {
-        fisr_num - second_num
-    } else if oprator == '/' {
-        fisr_num / second_num
-    } else if oprator == '*' {
-        fisr_num * second_num
-    } else {
-        0.0
+    match oprator {
+        '+' => fisr_num + second_num,
+        '-' => fisr_num - second_num,
+        '/' => fisr_num / second_num,
+        '*' | 'x' | 'X' => fisr_num * second_num,
+        _   => panic!("Invalid operator used.")
     }
+}
+
+fn output(fisr_num: f32, oprator: char, second_num: f32,result: f32) -> String {
+    format!("{} {} {} = {}",fisr_num,oprator,second_num,result)
 }
